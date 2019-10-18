c36e8fe63204c5768a2834575433075195e88cbf
Galaxy97 <atnyehin@gmail.com>
2019-10-18 15:37:23 +0300

    2 done

---
+++ js-check/2.js
@@ -0,0 +1,70 @@
+const user = {
+  firstName: "John", // string
+  lastName: "Doe", // string
+  rate: 0.86, // number in range 0..1
+  address: {
+    // not empty object or null
+    line1: "15 Macon St", // string
+    line2: "", // string
+    city: "Gotham" // string
+  },
+  phoneNumbers: [
+    // array containing at least 1 element
+    {
+      type: "MOBILE", // string, limited to MOBILE | LINE | VOIP
+      number: "(555) 555-1234" // string in specific format
+    },
+    {
+      type: "LINE",
+      number: "(555) 555-5678"
+    }
+  ]
+};
+
+const Ajv = require("ajv");
+const ajv = new Ajv({ allErrors: true });
+
+const schema = {
+  required: ["firstName", "lastName", "rate", "address", "phoneNumbers"],
+  properties: {
+    firstName: { type: "string" },
+    lastName: { type: "string" },
+    rate: { type: "number", minimum: 0, maximum: 1 },
+    address: {
+      type: "object",
+      required: ["line1", "line2", "city"],
+      properties: {
+        line1: { type: "string" },
+        line2: { type: "string" },
+        city: { type: "string" }
+      }
+    },
+    phoneNumbers: {
+      type: "array",
+      minItems: 1,
+      items: {
+        type: "object",
+        properties: {
+          type: {
+            type: "string",
+            enum: ["MOBILE", "LINE", "VOIP"]
+          },
+          number: {
+              type: "string",
+              pattern: "^\\(\\d{3}\\)\\s\\d{3}\\-\\d{4}$"
+          }
+        }
+      }
+    }
+  }
+};
+const validate = ajv.compile(schema);
+
+validator(user);
+
+function validator(data) {
+  const valid = validate(data);
+  if (valid) {
+    console.log("Valid!");
+  } else console.log("Invalid: " + ajv.errorsText(validate.errors));
+}
