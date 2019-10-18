8542d96b5f6156d91e39894d95e92bf5f181da94
Galaxy97 <atnyehin@gmail.com>
2019-10-18 17:46:06 +0300

    exercise 2 without ajs

--- js-check/2.js
+++ js-check/2.js
@@ -21,50 +21,97 @@ const user = {
   ]
 };
 
-const Ajv = require("ajv");
-const ajv = new Ajv({ allErrors: true });
-
-const schema = {
-  required: ["firstName", "lastName", "rate", "address", "phoneNumbers"],
-  properties: {
-    firstName: { type: "string" },
-    lastName: { type: "string" },
-    rate: { type: "number", minimum: 0, maximum: 1 },
-    address: {
-      type: "object",
-      required: ["line1", "line2", "city"],
-      properties: {
-        line1: { type: "string" },
-        line2: { type: "string" },
-        city: { type: "string" }
-      }
-    },
-    phoneNumbers: {
-      type: "array",
-      minItems: 1,
-      items: {
-        type: "object",
-        properties: {
-          type: {
-            type: "string",
-            enum: ["MOBILE", "LINE", "VOIP"]
-          },
-          number: {
-              type: "string",
-              pattern: "^\\(\\d{3}\\)\\s\\d{3}\\-\\d{4}$"
-          }
-        }
+function validateObject(user) {
+  if (typeof user.firstName !== "string" || typeof user.lastName !== "string") {
+    return false;
+  }
+  if (typeof user.rate !== "number" || (user.rate < 0 || user.rate > 1)) {
+    return false;
+  }
+  if (!user.address) {
+    return false;
+  } else {
+    for (key of Object.keys(user.address)) {
+      if (typeof user.address[key] !== "string") {
+        return false;
       }
     }
   }
-};
-const validate = ajv.compile(schema);
-
-validator(user);
-
-function validator(data) {
-  const valid = validate(data);
-  if (valid) {
-    console.log("Valid!");
-  } else console.log("Invalid: " + ajv.errorsText(validate.errors));
+  if (user.phoneNumbers.length < 1) {
+    return false;
+  } else {
+    for (element of user.phoneNumbers) {
+      if (typeof element.type !== "string") {
+        return false;
+      } else {
+        const defs = ["MOBILE", "LINE", "VOIP"];
+        const found = defs.find(function(elem) {
+          return elem == element.type;
+        });
+        if (!found) {
+          return false;
+        }
+      }
+      if (typeof element.number !== "string") {
+          return false;
+      } else {
+          if (!/^\(\d{3}\)\s\d{3}\-\d{4}$/.test(element.number)) {
+            return false;
+          }
+      }
+    }
+  }
+  return true;
 }
+
+if (validateObject(user)) {
+  console.log("valid");
+} else console.log("NOT valid");
+
+// const Ajv = require("ajv");
+// const ajv = new Ajv({ allErrors: true });
+
+// const schema = {
+//   required: ["firstName", "lastName", "rate", "address", "phoneNumbers"],
+//   properties: {
+//     firstName: { type: "string" },
+//     lastName: { type: "string" },
+//     rate: { type: "number", minimum: 0, maximum: 1 },
+//     address: {
+//       type: "object",
+//       required: ["line1", "line2", "city"],
+//       properties: {
+//         line1: { type: "string" },
+//         line2: { type: "string" },
+//         city: { type: "string" }
+//       }
+//     },
+//     phoneNumbers: {
+//       type: "array",
+//       minItems: 1,
+//       items: {
+//         type: "object",
+//         properties: {
+//           type: {
+//             type: "string",
+//             enum: ["MOBILE", "LINE", "VOIP"]
+//           },
+//           number: {
+//               type: "string",
+//               pattern: "^\\(\\d{3}\\)\\s\\d{3}\\-\\d{4}$"
+//           }
+//         }
+//       }
+//     }
+//   }
+// };
+// const validate = ajv.compile(schema);
+
+// validator(user);
+
+// function validator(data) {
+//   const valid = validate(data);
+//   if (valid) {
+//     console.log("Valid!");
+//   } else console.log("Invalid: " + ajv.errorsText(validate.errors));
+// }
