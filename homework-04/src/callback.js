21ee35141007d2b425f9e4cb1d4d633357d89964
Galaxy97 <atnyehin@gmail.com>
2019-12-03 21:32:27 +0200

    change callback

--- homework-04/src/callback.js
+++ homework-04/src/callback.js
@@ -1,33 +1,49 @@
-const timeArr = [700, 2000, 3000];
 let sum = 0;
 
-function throwDice() {
+function randomNumber() {
   return Math.floor(Math.random() * Math.floor(6));
 }
 
-function runner(callback) {
-  timeArr.forEach((time, index) => {
-    setTimeout(() => {
-      const num1 = throwDice();
-      const num2 = throwDice();
-      if (!num1 || !num2) {
-        callback(Error(`Lost dice`));
-      } else if (index === 2) {
-        callback(null, {num1, num2, last: true});
-      } else {
-        callback(null, {num1, num2});
-      }
-    }, time);
-  });
-}
-runner((e, result) => {
-  if (e) {
-    throw e;
-  }
-  sum += result.num1 + result.num2;
-  if (result.last) {
-    console.log(`sum is ${sum}`);
+function throwDice(error, num1, num2, end) {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(`throw one is ${result.num1} throw two is ${result.num2}`);
+    sum += num1 + num2;
+    if (end) {
+      console.log(`Sum is ${sum}`);
+    } else {
+      console.log(`number one is ${num1} number two is ${num2}`);
+    }
   }
-});
+}
+
+function runner(callback) {
+  setTimeout(() => {
+    let num1 = randomNumber();
+    let num2 = randomNumber();
+    if (num1 === 0 || num2 === 0) {
+      callback(new Error('Lost dise'));
+    } else {
+      callback(null, num1, num2);
+      setTimeout(() => {
+        num1 = randomNumber();
+        num2 = randomNumber();
+        if (num1 === 0 || num2 === 0) {
+          callback(new Error('Lost dise'));
+        } else {
+          callback(null, num1, num2);
+          setTimeout(() => {
+            num1 = randomNumber();
+            num2 = randomNumber();
+            if (num1 === 0 || num2 === 0) {
+              callback(new Error('Lost dise'));
+            } else {
+              callback(null, num1, num2, true);
+            }
+          }, 1000);
+        }
+      }, 1300);
+    }
+  }, 700);
+}
+runner(throwDice);
